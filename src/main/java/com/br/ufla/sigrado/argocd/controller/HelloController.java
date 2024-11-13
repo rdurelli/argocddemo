7f6e12e1d15b809c69c8ce035c67ec986b372c67
Rafael Durelli <rafael.durelli@ufla.br>
2024-11-13 08:37:47 -0300

    first commit

---
+++ src/main/java/com/br/ufla/sigrado/argocd/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.br.ufla.sigrado.argocd.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+public class HelloController {
+
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
